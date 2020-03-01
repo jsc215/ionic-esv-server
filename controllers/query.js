3782cab30dbcc634f5bfb6bf782a7e65d4c2913a
jsc215 <jeffcomertennis@gmail.com>
2020-03-01 15:14:05 -0500

    refactor search config into one object and add eslint and prettier

--- controllers/query.js
+++ controllers/query.js
@@ -1,22 +1,33 @@
 import axios from 'axios';
 import queryString from 'query-string';
+
 const api_url = process.env.API_URL;
 const api_key = process.env.API_KEY;
 
 export const search = async (req, res) => {
-  const searchType = req.query.searchType;
-  console.log(req.query);
-  const searchRequest = {
-    q: req.query.verse,
-    'include-footnotes': req.query.includeFootnotes,
-    'include-footnote-body': req.query.includeFootnoteBody,
-    'include-verse-numbers': req.query.includeVerseNumbers,
-    page: req.query.page,
+  const searchConfig = {
+    searchType: req.query.searchType,
+    searchRequest: {
+      q: req.query.verse,
+      'include-footnotes': req.query.includeFootnotes,
+      'include-footnote-body': req.query.includeFootnoteBody,
+      'include-verse-numbers': req.query.includeVerseNumbers,
+      page: req.query.page
+    }
   };
+  // const searchType = req.query.searchType;
+  // console.log(req.query);
+  // const searchRequest = {
+  //   q: req.query.verse,
+  //   'include-footnotes': req.query.includeFootnotes,
+  //   'include-footnote-body': req.query.includeFootnoteBody,
+  //   'include-verse-numbers': req.query.includeVerseNumbers,
+  //   page: req.query.page
+  // };
 
-  const searchParams = `${api_url}${searchType}/?` + queryString.stringify(searchRequest);
+  const searchParams = `${api_url}${searchConfig.searchType}/?${queryString.stringify(searchConfig.searchRequest)}`;
   try {
-    let result = await axios.get(searchParams, { headers: { Authorization: `Token ${api_key}` } });
+    const result = await axios.get(searchParams, { headers: { Authorization: `Token ${api_key}` } });
     if (
       (result.data.passages && !result.data.passages.length) ||
       (result.data.results && !result.data.results.length)
@@ -24,7 +35,7 @@ export const search = async (req, res) => {
       res.status(400).send('search returned no results');
       return;
     }
-    if (searchType === 'text') {
+    if (searchConfig.searchType === 'text') {
       res.json(result.data.passages[0].split('\n'));
     } else {
       console.log(result.data);
@@ -35,3 +46,5 @@ export const search = async (req, res) => {
     res.status(500).send(error);
   }
 };
+
+export default search;
